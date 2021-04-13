a35639e0970be811650086ca280f79fe7fb11588
Destoky <adestoky@gmail.com>
2021-04-13 23:07:16 +0200

    Images

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ let puissance4 = [];
 let nbrColonne = 7;
 let nbrLigne = 6;
 
+/**
+ * Permet d'initialiser un tableau Multidimensionnel
+ * @param {Number} nbrLigne 
+ * @param {Number} nbrColonne 
+ * @param {*} el 
+ * @returns 
+ */
 let initialiserTableauMultiDim = function (nbrLigne, nbrColonne, el) {
   let tabMulti = [];
   for (let i = 0; i < nbrLigne; i++) {
